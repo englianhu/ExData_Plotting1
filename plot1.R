f79302d6af96c3b28fc04e23aeb83b41a481679e
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-10 16:00:57 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -18,13 +18,11 @@ datetime <- paste(dat$Date,dat$Time)
 dat <- data.frame(Datetime=as.POSIXct(datetime), dat[-c(1:2)])
 class(dat$Datetime)
 file.remove(paste0(getwd(),'/data/',dir('data')))
-
 rm(data_dir, destfile, fileUrl, datetime)
 
 ## Plot 1
-hist(data$Global_active_power, main="Global Active Power",
-xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+hist(dat$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-## Saving to file
+## Save to file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()

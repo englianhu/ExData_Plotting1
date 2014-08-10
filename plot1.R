6d58ecdcb5a1be941e843778e6868aff267905f1
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-10 15:47:22 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,22 +3,28 @@ if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 destfile <- "household_power_consumption.zip"
 download.file(fileUrl, destfile=paste("data", destfile, sep="/"))
+
+## Unzip the dataset
 unzip(paste("data", destfile, sep="/"), exdir="data")
 data_dir <- setdiff(dir("data"), destfile)
 
-## Read txt dataset
-dat <- read.csv("./data/household_power_consumption.txt", header=T, sep=';')
+## Read and subset the txt dataset
+dat <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259)
 dat$Date <- as.Date(dat$Date, format="%d/%m/%Y")
+dat <- subset(dat, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data)
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+## Convert date/time to datetime class
+datetime <- paste(dat$Date,dat$Time)
+dat <- data.frame(Datetime=as.POSIXct(datetime), dat[-c(1:2)])
+class(dat$Datetime)
+file.remove(paste0(getwd(),'/data/',dir('data')))
+
+rm(data_dir, destfile, fileUrl, datetime)
+
 ## Plot 1
 hist(data$Global_active_power, main="Global Active Power",
 xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
 ## Saving to file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
